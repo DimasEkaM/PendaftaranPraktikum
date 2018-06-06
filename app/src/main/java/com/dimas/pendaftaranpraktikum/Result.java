43de21f6a9725779fa8ea9449e6278758e5e6454
DimasEkaM <mahendradimas21@gmail.com>
2018-06-06 10:30:05 +0700

    Initial commit

---
+++ app/src/main/java/com/dimas/pendaftaranpraktikum/Result.java
@@ -0,0 +1,29 @@
+package com.dimas.pendaftaranpraktikum;
+
+/**
+ * Created by Lenovo on 05-Jun-18.
+ */
+
+public class Result {
+
+    String npm;
+    String nama;
+    String kelas;
+    String sesi;
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public String getSesi() {
+        return sesi;
+    }
+}
---
+++ app/src/main/java/com/dimas/pendaftaranpraktikum/Value.java
@@ -0,0 +1,26 @@
+package com.dimas.pendaftaranpraktikum;
+
+import java.util.List;
+
+/**
+ * Created by Lenovo on 05-Jun-18.
+ */
+
+public class Value {
+    String value;
+    String message;
+    List<Result> result;
+
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<Result> getResult() {
+        return result;
+    }
+}
